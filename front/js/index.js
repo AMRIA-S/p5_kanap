c374c24438533b48f38241ad6e6573327b638904
amria <amria.sollene@gmail.com>
2023-04-28 12:10:00 +0200

    Etape 4 et 5 à refaire

--- front/js/index.js
+++ front/js/index.js
@@ -8,25 +8,28 @@ console.log(items);
 
 }
 
-function insertProducts(products) {
-    
-    const parser = new DOMParser();
-    const items = document.getElementById('items');
-    for (let i = 0; i < products.length; i++) {
-        let productsItems = //We insert the HTML in the DOM
-            `<a href="./product.html?id=${products[i]._id}"> 
-            <article>            
-            <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
-            <h3 class="productName">${products[i].name}</h3>
-            <p class="productDescription">${products[i].description}</p>
-            </article>
-            </a>`;
-        const displayItems = parser.parseFromString(productsItems, "text/html");
-        items.appendChild(displayItems.body.firstChild);
-    };
-}
-getProduct();
+/**function insertProducts(products) {
+*    
+*    const parser = new DOMParser();
+*    const items = document.getElementById('items');
+*    for (let i = 0; i < products.length; i++) {
+*        let productsItems = //We insert the HTML in the DOM
+*            `<a href="./product.html?id=${products[i]._id}"> 
+*            <article>            
+*            <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+*            <h3 class="productName">${products[i].name}</h3>
+*            <p class="productDescription">${products[i].description}</p>
+*            </article>
+*            </a>`;
+*        const displayItems = parser.parseFromString(productsItems, "text/html");
+*        items.appendChild(displayItems.body.firstChild);
+*    };
+*}
+*getProduct();*/
 
 
 
-
+function choiceColor (colors) {
+    document.getElementById("colors").style.backgroundColor = colors;
+    return 0;
+  }
--- front/js/product.js
+++ front/js/product.js
@@ -1,13 +1,21 @@
-fetch('http://localhost:3000/api/products').then((res) =>
+const apiID = '{id=42}';
+
+fetch('http://localhost:3000/api/products?${apiId}').then((res) =>
 res.json().then((kanap) => {
     console.log(kanap);
-    document.querySelector("img").innerHTML = kanap.img;
-    document.querySelector('#price').innerHTML = kanap.price;
-    document.querySelector('#title').innerHTML = kanap.name;
-    document.querySelector('#description').innerHTML = kanap.description;
-    document.querySelector('#colors').innerHTML = kanap.colors;
-    document.querySelector('#quantity').innerHTML = kanap.quantity;
     
 
-    })
+    function kanapPageProduct() {
+        for (i = 0; kanap.length; i++) {
+            
+            document.getElementsByTagName("img").innerHTML = kanap.img;
+            document.getElementById('price').innerHTML = kanap.price;
+            document.getElementById('title').innerHTML = kanap.name;
+            document.getElementById('#description').innerHTML = kanap.description;
+            document.getElementById('#colors').innerHTML = kanap.colors;
+
+            
+        }
+    }
+})
 );
